2c26eee851bb9de54d5b9e967e94a0c8a7b2a9b5
Istiyak Ahmed <iamistiyakooo@gmail.com>
2025-04-21 22:34:19 +0600

    update

---
+++ BFS.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bfs(int start, vector<vector<int>> &adj, int n)
+{
+    vector<bool> visited(n + 1, false);
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        cout << u << " ";
+        for (int v : adj[u])
+        {
+            if (!visited[v])
+            {
+                visited[v] = true;
+                q.push(v);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> adj(n + 1);
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    int start;
+    cin >> start;
+    bfs(start, adj, n);
+}
---
+++ DFS.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void dfs(int node, vector<vector<int>> &adj, vector<bool> &visited)
+{
+    visited[node] = true;
+    cout << node << " ";
+    for (int neighbor : adj[node])
+    {
+        if (!visited[neighbor])
+        {
+            dfs(neighbor, adj, visited);
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    vector<vector<int>> adj(n + 1);
+    for (int i = 0; i < e; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    vector<bool> visited(n + 1, false);
+    dfs(1, adj, visited);
+}
